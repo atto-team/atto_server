6b5ef16247dfe3504f05643618fc42f9fcd28d5d
Gyejoon <00700mm@gmail.com>
2019-08-12 02:22:38 +0900

    SecurityConfig 세팅

--- nimontoy-security/src/main/kotlin/com/atto/nimontoy/config/SecurityConfig.kt
+++ nimontoy-security/src/main/kotlin/com/atto/nimontoy/config/SecurityConfig.kt
@@ -1,6 +1,22 @@
 package com.atto.nimontoy.config
 
+import com.atto.nimontoy.AuthenticationFilter
+import com.atto.nimontoy.UnauthorizedAuthenticationEntryPoint
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.BeanIds
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+import java.lang.Exception
 
 /**
  * Created by 00700mm@gmail.com on 2019-07-18
@@ -8,6 +24,69 @@ import org.springframework.context.annotation.Configuration
  * Github : http://github.com/Gyejoon
  */
 @Configuration
-class SecurityConfig {
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
+class SecurityConfig(
+        private val unauthorizedHandler: UnauthorizedAuthenticationEntryPoint
+) : WebSecurityConfigurerAdapter() {
+
+    @Bean
+    fun authenticationFilter() = AuthenticationFilter()
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Throws(Exception::class)
+    override fun authenticationManager(): AuthenticationManager {
+        return super.authenticationManager()
+    }
+
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.addAllowedOrigin("*")
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("OPTIONS")
+        config.addAllowedMethod("HEAD")
+        config.addAllowedMethod("GET")
+        config.addAllowedMethod("PUT")
+        config.addAllowedMethod("POST")
+        config.addAllowedMethod("DELETE")
+        config.addAllowedMethod("PATCH")
+        source.registerCorsConfiguration("/**", config)
+        return CorsFilter(source)
+    }
+
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http
+                .addFilter(corsFilter())
+                .csrf()
+                .disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(unauthorizedHandler)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS)
+                .permitAll()
+                .antMatchers("/",
+                        "/favicon.ico",
+                        "/**/*.png",
+                        "/**/*.gif",
+                        "/**/*.svg",
+                        "/**/*.jpg",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+
+        // Add our custom JWT security filter
+        http.addFilterBefore(authenticationFilter(), UsernamePasswordAuthenticationFilter::class.java)
+    }
 
 }
